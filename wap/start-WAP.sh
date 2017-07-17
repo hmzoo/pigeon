5d0f291d702c0ac2da05b0874a5fb0afe87d4753
HMJ <hmj@goatpage.com>
2017-07-17 20:34:58 +0000

    cleanning

--- wap/start-WAP.sh
+++ wap/start-WAP.sh
@@ -4,17 +4,18 @@
 
 INT_WIFI="wlan0"
 INT_NET="eth0"
+SSID="WIFISPOT"
+PASS_PHRASE="LastWhiteN8"
+
 
 IP="192.168.33.1"
 CID="24"
 NETWORK="192.168.33.0/24"
 DHCP_RANGE="192.168.33.100,192.168.33.254,255.255.255.0,4h"
-
-SSID="PIWIFI"
 CHANNEL="6"
-PASS_PHRASE="LastWhiteN8"
 DRIVER=nl80211
 
+#NETWORK
 ip addr add $IP/$CID dev $INT_NET
 sleep 3
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -26,6 +27,8 @@ iptables -A FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT
 iptables -A FORWARD -i $INT_WIFI --destination $NETWORK --match state --state NEW --jump ACCEPT
 iptables -A INPUT -s $NETWORK --jump ACCEPT
 
+
+#DNSMASQ CONFIG
 cat <<EOT > /tmp/dnsmasq.conf
 domain-needed
 bogus-priv
@@ -33,6 +36,7 @@ interface=$INT_WIFI
 dhcp-range=$DHCP_RANGE
 EOT
 
+#HOSTAPD CONFIG
 cat <<EOT > /tmp/hostapd.conf
 interface=$INT_WIFI
 driver=$DRIVER
@@ -56,10 +60,12 @@ echo "STARTING HOSTAPD"
 hostapd   /tmp/hostapd.conf 
 
 #CLEANING
-killall dnsmasq
+killall dnsmasq hostapd
 iptables -D POSTROUTING -t nat -o $INT_NET -j MASQUERADE
 iptables -D FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT
 iptables -D FORWARD -i $INT_WIFI --destination $NETWORK --match state --state NEW --jump ACCEPT
 iptables -D INPUT -s $NETWORK --jump ACCEPT
+
+ip addr flush dev $INT_WIFI
 
 echo -e "\nTERMINATED\n\n" 
