1c18e3fb187f0590311968446d22270a21c8576f
HMJ <hmj@goatpage.com>
2017-07-17 20:19:27 +0000

    ok

--- install-WAP.sh
+++ install-WAP.sh
@@ -2,4 +2,8 @@
 
 
 apt-get install dnsmasq hostapd
+update-rc.d dnsmasq disable
+update-rc.d hostapd disable
+sed -i -e 's/ENABLED=1/ENABLED=0/' /etc/default/dnsmasq
 
+  
--- start-WAP.sh
+++ start-WAP.sh
@@ -7,18 +7,24 @@ INT_NET="eth0"
 
 IP="192.168.33.1"
 CID="24"
-MASK="255.255.255.0"
-
+NETWORK="192.168.33.0/24"
 DHCP_RANGE="192.168.33.100,192.168.33.254,255.255.255.0,4h"
 
 SSID="PIWIFI"
 CHANNEL="6"
-PASS_PHRASE="LastWhiteNight"
+PASS_PHRASE="LastWhiteN8"
 DRIVER=nl80211
 
-ip addr add $IP/$CID dev $INT_WIFI
+ip addr add $IP/$CID dev $INT_NET
+sleep 3
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+modprobe ipt_MASQUERADE
+
+iptables -A POSTROUTING -t nat -o $INT_NET -j MASQUERADE
+iptables -A FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT
+iptables -A FORWARD -i $INT_WIFI --destination $NETWORK --match state --state NEW --jump ACCEPT
+iptables -A INPUT -s $NETWORK --jump ACCEPT
 
 cat <<EOT > /tmp/dnsmasq.conf
 domain-needed
@@ -39,5 +45,21 @@ wpa=3
 wpa_passphrase=$PASS_PHRASE
 EOT
 
-dnsmasq -x /var/run/dnsmasq.pid -C /tmp/dnsmasq.conf
-hostapd -B -P /var/run/hostapd.pid /tmp/hostapd.conf
+
+#STARTING
+killall dnsmasq
+killall hostapd
+echo -e "INTERFACE: $INT_WIFI \nSSID:$SSID Channel:$CHANNEL\nKEY: $PASS_PHRASE\n"
+echo "STARTING DNSMASQ"
+dnsmasq   -C /tmp/dnsmasq.conf &
+echo "STARTING HOSTAPD" 
+hostapd   /tmp/hostapd.conf 
+
+#CLEANING
+killall dnsmasq
+iptables -D POSTROUTING -t nat -o $INT_NET -j MASQUERADE
+iptables -D FORWARD --match state --state RELATED,ESTABLISHED --jump ACCEPT
+iptables -D FORWARD -i $INT_WIFI --destination $NETWORK --match state --state NEW --jump ACCEPT
+iptables -D INPUT -s $NETWORK --jump ACCEPT
+
+echo -e "\nTERMINATED\n\n" 
